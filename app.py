92798d66ec63cd66686ef07c559a893a655b16a3
mrm <mrm.sary@yahoo.com>
2020-02-25 11:25:58 +0330

    flask initial app

--- app.py
+++ app.py
@@ -5,3 +5,16 @@ app = Flask(__name__)
 @app.route("/") 
 def main_page():
     return  "hello" 
+
+@app.route("/v1/process")
+def process():
+    pass
+
+def send_sms():
+    pass
+
+def check_serial():
+    pass
+
+if __name__ == "--main--":
+    app.run("0.0.0.0",5000)
