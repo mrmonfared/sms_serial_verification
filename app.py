b3073f58bfc755476615509a6a4e40349161ed67
mehdi <mrm.sary@yahoo.com>
2020-03-16 20:45:38 +0330

    send and recieve sms works

--- app.py
+++ app.py
@@ -1,20 +1,31 @@
-from flask import Flask
+from flask import Flask,jsonify,request
+import requests
+import config
+
+
 app = Flask(__name__)
 
 
-@app.route("/") 
-def main_page():
-    return  "hello" 
-
-@app.route("/v1/process")
+@app.route("/", methods =["GET","POST"])
 def process():
-    pass
+    data=request.form
+    sender=data['from']
+    message =data['message']
+    print('received {} from {}'.format(message,sender))
+    send_sms(sender,message)
+    ret = {"message":"processed"}
+    return jsonify(ret) , 200
 
-def send_sms():
-    pass
+def send_sms(receptor,message):
+    url = 'https://api.kavenegar.com/v1/{}/sms/send.json'.format(config.API_KEY)
+    print(config.API_KEY)
+    data ={'message':message ,
+           'receptor':receptor }
+    res = requests.post(url,data)     
+    print("message :{} status code:{}".format(message,res.status_code))  
 
 def check_serial():
     pass
 
-if __name__ == "--main--":
+if __name__ == "__main__":  
     app.run("0.0.0.0",5000)
